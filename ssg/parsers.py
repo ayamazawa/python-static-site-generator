a978a638e3a795f630fa4deb9aa5a4cadd03b058
Adriano Yamazawa <adriano.yamazawa@gmail.com>
2020-07-27 15:33:33 -0300

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -38,19 +38,23 @@ class ResourceParser(Parser):
         def parse(self, path, source, dest):
             self.copy(path, source, dest)
             
-class MarkdownParser():
+class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
-    def parse(self, path: Path, source: Path, dest: Path):
+
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(html,path,dest)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n")
+        self.write(path,dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
-class ReStructuredTextParser():
+class ReStructuredTextParser(Parser):
     extensions = [".rst"]
-    def parse(self, path: Path, source: Path, dest: Path):
+
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body,writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
 
--- ssg/site.py
+++ ssg/site.py
@@ -24,7 +24,7 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            self.error("No parser for the {} extension, file skipped!")
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
     
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
@@ -35,4 +35,4 @@ class Site:
                 self.run_parser(path)
     @staticmethod
     def error(message):
-        sys.stderr.write("\x1b[1;31m{}\n")
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
