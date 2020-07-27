30df2fb274af64d3b91e0af550436c79849d10e2
Adriano Yamazawa <adriano.yamazawa@gmail.com>
2020-07-27 14:11:46 -0300

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,34 +1,37 @@
-from typing import List
-from pathlib import Path
 import shutil
 
+from typing import List
+from pathlib import Path
+
+
 class Parser:
-    extensions = List[str]
+    extensions: List[str] = []
+
     def valid_extension(self, extension):
-        if extension is in self.extensions:
-            return extension
-    
-    def parse(path, source, dest):
+        return extension in self.extensions
+        
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-    def read(path):
-        with open(path):
-            return read(file)
-    
-    def write(path,dest,content,ext=".html"):
-        full_path = "self.dest/with_suffix(ext).name"
-        return full_path
-        with open(full_path):
-            content = write(file)
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
 
-    def copy(path,source,dest):
-        shutil.copy2(path,full_path)
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
+            
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
     
-    def ResourceParser():
-        self.extensions = ["*.jpg", ".png", ".gif", ".css", ".html"]
-        parse(self)
-        copy(path,source,dest)
-        
+class ResourceParser(Parser):
+        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+        def parse(self, path, source, dest):
+            self.copy(path, source, dest)
+            
 
 
 
